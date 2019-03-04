4e72dc741c5ed2f0d18000a4540f606b00c67463
deepweaver <godeepweaver@gmail.com>
2019-03-04 15:52:15 -0500

    added

--- vae.py
+++ vae.py
@@ -7,7 +7,10 @@ import torch
 # from torchvision import datasets, transforms 
 # from torchvision.utils import save_image 
 import torchvision 
-
+import os, sys 
+if not os.path.isdir("./results"):
+    os.system("mkdir results")
+    
 parser = argparse.ArgumentParser(description='VAE MNIST') 
 parser.add_argument('--batch-size', type=int, default=128, metavar='N', \
     help='input batch size for training (default=128)')
